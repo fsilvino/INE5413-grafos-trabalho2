8adb67cae61eda9b8bd4b0cd2f4cd388cba667a6
Renan <renanjochem98@gmail.com>
2020-10-18 18:55:53 -0300

    Herdando da classe Relacoes

--- Aresta.py
+++ Aresta.py
@@ -1,14 +1,9 @@
-class Aresta:
+from Relacao import Relacao
+class Aresta(Relacao):
 
-    def __init__(self, v1, v2, peso):
-        self.uid = Aresta.gerarIdAresta(v1, v2)
-        self.v1 = v1
-        self.v2 = v2
-        self.peso = peso
+
 
     def obterOutraParte(self, v):
         return self.v1 if self.v1.numero != v.numero else self.v2
 
-    def gerarIdAresta(v1, v2):
-        return f'{min(v1.numero, v2.numero)}.{max(v1.numero, v2.numero)}'
-    gerarIdAresta = staticmethod(gerarIdAresta)
+    
