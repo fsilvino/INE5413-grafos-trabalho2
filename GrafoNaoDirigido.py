823f1e11dce50fdc4dee1d815d1170e3c65c338e
Flávio Silvino <fsilvino@users.noreply.github.com>
2020-10-18 23:16:33 -0300

    Alterações na estrutura

---
+++ GrafoNaoDirigido.py
@@ -0,0 +1,17 @@
+from Grafo import Grafo
+from Aresta import Aresta
+
+class GrafoNaoDirigido(Grafo):
+
+    def gerarIdRelacao(self, v1, v2):
+        return f'{min(v1.numero, v2.numero)}.{max(v1.numero, v2.numero)}'
+
+    def adicionarRelacao(self, v1, v2, peso):
+        id = self.gerarIdRelacao(v1, v2)
+        relacao = Aresta(id, v1, v2, peso)
+        self.relacoes[id] = relacao
+        self.vertices[v1].adicionarRelacao(relacao)
+        self.vertices[v2].adicionarRelacao(relacao)
+
+    def obterTipoGrafo(self):
+        return "Grafo não-dirigido"
